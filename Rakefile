c0095eefc69fe8aa498c26870cea875f54d7cc4c
Dirk Breuer <dirk.breuer@gmail.com>
2016-11-19 03:42:38 +0100

    define options to select from for arguments

--- Rakefile
+++ Rakefile
@@ -6,6 +6,13 @@ require "uri"
 
 config = YAML.load_file(".config")
 
+def options_for(argument)
+  case argument
+  when :channel
+    %w(@dirk @bascht)
+  end
+end
+
 desc "Prints the current time in `/tmp/webrake`"
 task :update_timestamp do
   File.open("/tmp/webrake", "w+") do |f|
@@ -18,6 +25,16 @@ task :notify, %i(notification) do |_t, args|
   slack_payload = {
     text: args[:notification],
     channel: "@dirk"
+  }
+
+  Net::HTTP.post_form(URI(config["slack_webhook_url"]), payload: slack_payload.to_json)
+end
+
+desc "Send a Notification with predefined options"
+task :notify_with_option, %i(notification channel) do |_t, args|
+  slack_payload = {
+    text: args[:notification],
+    channel: args[:channel]
   }
 
   Net::HTTP.post_form(URI(config["slack_webhook_url"]), payload: slack_payload.to_json)
--- app.rb
+++ app.rb
@@ -18,10 +18,30 @@ rake_app = Rake.application
 rake_app.init
 rake_app.load_rakefile
 
+class TaskWrapper < SimpleDelegator
+  def has_options_for?(arg_name)
+    !!options[arg_name.to_sym]
+  end
+
+  def options
+    arg_names.each_with_object({}) do |arg_name, options|
+      if __getobj__.respond_to?(:options_for, true)
+        if options_for_arg_name = __getobj__.send(:options_for, arg_name)
+          options[arg_name.to_sym] = options_for_arg_name
+        else
+          next
+        end
+      else
+        next
+      end
+    end
+  end
+end
+
 get "/" do
   notice = session.delete(:notice)
 
-  haml :index, format: :html5, locals: { tasks: rake_app.tasks, notice: notice }
+  haml :index, format: :html5, locals: { tasks: rake_app.tasks.map { |task| TaskWrapper.new(task) }, notice: notice }
 end
 
 rake_app.tasks.each do |task|
